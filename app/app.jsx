5e7423f52c1bb5915dd986e0b53be1aa66e5c99d
Lawrence Francell <lawrence@twintails.com>
2016-10-05 08:42:34 -0500

    All the Things!

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+const React = require('react'),
+      ReactDOM = require('react-dom')
+
+
+var firstName = 'Dave';
+
+ReactDOM.render(
+  <h1>Boilerplate App</h1>,
+  document.getElementById('app')
+);
